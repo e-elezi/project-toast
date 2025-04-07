4680e25a3db08329be605fe933cfd5330f3f2170
Enkeleda Elezi <enkeleda.elezi@gmail.com>
2025-04-07 20:45:04 +0200

    Toast component has props

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 import {
   AlertOctagon,
   AlertTriangle,
   CheckCircle,
   Info,
   X,
-} from 'react-feather';
+} from "react-feather";
 
-import VisuallyHidden from '../VisuallyHidden';
+import VisuallyHidden from "../VisuallyHidden";
 
-import styles from './Toast.module.css';
+import styles from "./Toast.module.css";
+import { VARIANT_OPTIONS } from "../constants";
 
 const ICONS_BY_VARIANT = {
   notice: Info,
@@ -18,16 +19,19 @@ const ICONS_BY_VARIANT = {
   error: AlertOctagon,
 };
 
-function Toast() {
+function Toast({ handleDismiss, variant, content }) {
+  if (!VARIANT_OPTIONS.includes(variant)) {
+    throw Error(`The only possible variants are ${VARIANT_OPTIONS}`);
+  }
+
+  const IconTag = ICONS_BY_VARIANT[variant];
   return (
-    <div className={`${styles.toast} ${styles.notice}`}>
+    <div className={`${styles.toast} ${styles[variant]}`}>
       <div className={styles.iconContainer}>
-        <Info size={24} />
+        <IconTag size={24} />
       </div>
-      <p className={styles.content}>
-        16 photos have been uploaded
-      </p>
-      <button className={styles.closeButton}>
+      <p className={styles.content}>{content}</p>
+      <button className={styles.closeButton} onClick={handleDismiss}>
         <X size={24} />
         <VisuallyHidden>Dismiss message</VisuallyHidden>
       </button>
